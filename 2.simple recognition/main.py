bbe792a03723122b2d2c5c0aa7dd4973c817113c
Mingxi Cao <mxcao_alina@outlook.com>
2023-07-19 20:21:53 +0800

    create sub-folder

--- 2.simple recognition/main.py
+++ 2.simple recognition/main.py
@@ -39,17 +39,31 @@ def transcribe(audio_url):
     return job_id
 
 
-audio_url = upload(filename)
-transcript_id = transcribe(audio_url)
 
-print(transcript_id)
+
+
 # poll
+def poll(transcript_id):
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    polling_response = requests.get(polling_endpoint, headers=headers)
+    return polling_response.json()
 
-polling_endpoint = transcript_endpoint + '/' + transcript_id
-polling_response = requests.get(polling_endpoint, headers=headers)
-print(polling_response.json())
+
+def get_transcription_result_url(audio_url):
+    transcript_id = transcribe(audio_url)
+
+    while True:
+        data = poll(transcript_id)
+        if data['status'] == 'completed':
+            return data, None
+        elif data['status'] == 'error':
+            return data, data['error']
 
 
+audio_url = upload(filename)
+data, error = get_transcription_result_url(audio_url)
+
+print (data)
 
 # save transcript
 
